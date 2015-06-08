c4d79e4ded73a4bde3170d81545ebdd00226fb0f
Ernest Micklei <ernest.micklei@gmail.com>
2015-06-08 20:47:50 +0200

    dump,convert and read from HOME/sounds
    Change-Id: I01143dda366568ba447af41b0d2827f0870b22b9

--- sounds/multiplayerpiano.go
+++ sounds/multiplayerpiano.go
@@ -5,11 +5,14 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 // Check the license of multiplayerpiano.com before fetching their WAV-es.
 
 func main() {
+	dir := filepath.Join(os.Getenv("HOME"), "sounds")
+	fmt.Println("writing sounds in", dir)
 	base := "http://multiplayerpiano.com/mp3/"
 	notes := []string{"c", "cs", "d", "ds", "e", "f", "fs", "g", "gs", "a", "as", "b"}
 	for oct := 1; oct < 7; oct++ {
@@ -19,7 +22,7 @@ func main() {
 			fmt.Printf("fetching:%s\n", prefix)
 			defer resp.Body.Close()
 			body, _ := ioutil.ReadAll(resp.Body)
-			ioutil.WriteFile("/tmp/"+prefix+".wav.mp3", body, os.ModePerm)
+			ioutil.WriteFile(filepath.Join(dir, prefix+".wav.mp3"), body, os.ModePerm)
 		}
 	}
 }
