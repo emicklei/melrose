e59b9f42876df156904ec70a416f82d4e5e134ab
emicklei_kramphub <ernest@kramphub.com>
2020-04-18 00:30:33 +0200

    mutex protect RunString of JS

--- js/vm.go
+++ js/vm.go
@@ -2,8 +2,11 @@ package js
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"sync"
 
 	"github.com/dop251/goja"
 	"github.com/emicklei/melrose"
@@ -40,12 +43,13 @@ func NewVirtualMachine() *goja.Runtime {
 // LanguageServer can execute DSL statements received over HTTP
 type LanguageServer struct {
 	vm      *goja.Runtime
+	mutex   *sync.Mutex
 	address string
 }
 
 // NewLanguageServer returns a new LanguageService. It is not started.
 func NewLanguageServer(vm *goja.Runtime, addr string) *LanguageServer {
-	return &LanguageServer{vm: vm, address: addr}
+	return &LanguageServer{vm: vm, address: addr, mutex: new(sync.Mutex)}
 }
 
 // Start will start a HTTP listener for serving DSL statements
@@ -60,22 +64,58 @@ func (l *LanguageServer) statementHandler(w http.ResponseWriter, r *http.Request
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+	r.ParseForm()
+	trace := r.FormValue("trace") == "true"
 	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	defer r.Body.Close()
+	// the vm is for single thread usage
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
 	returnValue, err := l.vm.RunString(string(data))
 	var response interface{}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		notify.Print(notify.Errorf("JS error:%v", err))
-		response = err
+		notify.Print(notify.Errorf("melrose.js:%s", err.Error()))
+		if jserr, ok := err.(*goja.Exception); ok {
+			response = errorFrom(jserr)
+		} else {
+			response = errorFrom(err)
+		}
 	} else {
-		response = returnValue.Export()
+		if gov := returnValue.Export(); gov != nil {
+			response = resultFrom(gov)
+		}
 	}
+	w.Header().Set("content-type", "application/json")
 	enc := json.NewEncoder(w)
 	enc.SetIndent("", "\t")
 	enc.Encode(response)
+	if trace {
+		log.Printf("[melrose.trace] %#v\n", response)
+	}
+}
+
+type evaluationResult struct {
+	Type   string `json:"type"`
+	Object interface{}
+}
+
+func resultFrom(val interface{}) evaluationResult {
+	return evaluationResult{Type: fmt.Sprintf("%T", val), Object: val}
+}
+
+type evaluationError struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+	Line    int    `json:"line"`
+	Column  int    `json:"column"`
+}
+
+// ReferenceError: bpm is not defined at <eval>:1:4(3)
+func errorFrom(err error) evaluationError {
+	return evaluationError{Type: fmt.Sprintf("%T", err), Message: err.Error()}
 }
