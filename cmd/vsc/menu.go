f66cc383c62d3ae8a018d9630a638c5a9b5ffb49
Ernest Micklei <ernest.micklei@gmail.com>
2020-07-18 18:33:38 +0200

    add missing code

---
+++ cmd/vsc/menu.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func postProcessMenus() {
+	data, err := ioutil.ReadFile("../../docs/menu.template")
+	checkErr(err)
+	mds, err := filepath.Glob("../../docs/*.md")
+	checkErr(err)
+	for _, each := range mds {
+		postProcessMenu(each, string(data))
+	}
+}
+
+func postProcessMenu(file string, menu string) {
+	data, err := ioutil.ReadFile(file)
+	checkErr(err)
+	replaced := strings.Replace(string(data), "$$menu", menu, -1)
+	err = ioutil.WriteFile(file, []byte(replaced), os.ModePerm)
+	checkErr(err)
+}
---
+++ docs/tutorial/02-sequence.md
@@ -0,0 +1,10 @@
+---
+title: Melrōse Tutorial 02 Sequence
+---
+
+## create a sequence
+
+
+
+[Prev: 01 Note](01-note.html)
+[Next: 03 Chord](03-chord.html)
