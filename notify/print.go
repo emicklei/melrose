419a9ae4ebf8e02633a40dc9ebd38dfb978e5401
Ernest Micklei <ernest.micklei@gmail.com>
2020-11-28 09:23:34 +0100

    restore term colors

--- notify/print.go
+++ notify/print.go
@@ -19,17 +19,20 @@ func Print(m Message) {
 	}
 }
 
-func printInfo(args ...interface{}) {
-	fmt.Fprintf(Console.StandardOut, "INFO: %s\n", args...)
-}
+// Println is to inject a function that can report info,error and warning
+var Println = fmt.Println
 
-func printError(args ...interface{}) {
-	fmt.Fprintf(Console.StandardError, "ERROR: %s\n", args...)
-}
+// func printInfo(args ...interface{}) {
+// 	fmt.Fprintf(Console.StandardOut, "INFO: %s\n", args...)
+// }
 
-func printWarning(args ...interface{}) {
-	fmt.Fprintf(Console.StandardOut, "WARN: %s\n", args...)
-}
+// func printError(args ...interface{}) {
+// 	fmt.Fprintf(Console.StandardError, "ERROR: %s\n", args...)
+// }
+
+// func printWarning(args ...interface{}) {
+// 	fmt.Fprintf(Console.StandardOut, "WARN: %s\n", args...)
+// }
 
 func Debugf(format string, args ...interface{}) {
 	// make sure it ends with newline
@@ -37,4 +40,16 @@ func Debugf(format string, args ...interface{}) {
 		format += "\n"
 	}
 	fmt.Fprintf(Console.StandardOut, format, args...)
+}
+
+func printInfo(args ...interface{}) {
+	Println(append([]interface{}{"\033[1;32minfo:\033[0m"}, args...)...)
+}
+
+func printError(args ...interface{}) {
+	Println(append([]interface{}{"\033[1;31merror:\033[0m"}, args...)...)
+}
+
+func printWarning(args ...interface{}) {
+	Println(append([]interface{}{"\033[1;33mwarning:\033[0m"}, args...)...)
 }
