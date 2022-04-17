37f8d7c9b36bc165fd311114097e6b5da7a51b68
Ernest Micklei <ernest.micklei@gmail.com>
2022-04-17 19:56:49 +0200

    move pkg scripts to melrose-osx

--- packaging/macosx/post-install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ln -s  /Applications/Melrose/run.sh /usr/local/bin/melrose
--- packaging/macosx/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Melrōse startup script
-
-export DYLD_FALLBACK_LIBRARY_PATH=/Applications/Melrose && /Applications/Melrose/melrose
