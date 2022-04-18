cdf6bec3eba40a6e7c5a814426c079103f8309cd
Ernest Micklei <ernest.micklei@gmail.com>
2022-04-18 10:09:15 +0200

    octave test + tie

--- op/octave_test.go
+++ op/octave_test.go
@@ -1,15 +1,16 @@
 package op
 
 import (
-	"github.com/emicklei/melrose/core"
 	"testing"
+
+	"github.com/emicklei/melrose/core"
 )
 
 func TestOctave(t *testing.T) {
-	s1 := core.MustParseSequence("C D E")
+	s1 := core.MustParseSequence("C D E~8E")
 	s2 := core.MustParseSequence("F G A")
 	o := Octave{Target: []core.Sequenceable{s1, s2}, Offset: core.On(1)}
-	if got, want := o.S().Storex(), "sequence('C5 D5 E5 F5 G5 A5')"; got != want {
+	if got, want := o.S().Storex(), "sequence('C5 D5 E5~8E5 F5 G5 A5')"; got != want {
 		t.Errorf("got [%v] want [%v]", got, want)
 	}
 }
