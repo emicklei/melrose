d95e4b1a0a9982972252cc800b023283808697b6
Ernest Micklei <ernest.micklei@gmail.com>
2024-04-22 15:47:02 +0200

    replace ioutlil

--- server/statements.go
+++ server/statements.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strconv"
 	"strings"
@@ -34,7 +34,7 @@ func (l *LanguageServer) statementHandler(w http.ResponseWriter, r *http.Request
 		}
 	}
 	// get expression source
-	data, err := ioutil.ReadAll(r.Body)
+	data, err := io.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
